e26e52bd2c18198c55e8558be89fd3247bbfa212
Dmitriy Belousov <d-belosov@hotmail.com>
2025-02-26 08:40:02 +0500

    feat: добавит id

--- server.cjs
+++ server.cjs
@@ -1,18 +1,24 @@
-const express= require("express");
-const webs= require("ws")
+const express=require("express");
+const webSocket= require("ws");
 const http = require("http");
 
 
 const app = express();
 const server = http.createServer(app);
-const wss = new webs.WebSocketServer({ server });
+const wss = new webSocket.WebSocketServer({ server });
+const clients= new Map()
 
 wss.on("connection", (ws) => {
   console.log("Клиент подключен");
-
+  const id=Math.round(Math.random()*1000);
+clients.set(id,ws);
   ws.on("message", (message) => {
+    const data=JSON.parse(message);
+    const target=clients.get(data.id);
+    if (target){
+      target.send(JSON.stringify(JSON.stringify({...data, from: id})));
+    }
     console.log(`Получено сообщение: ${message}`);
-    ws.send(`Эхо: ${message}`);
   });
 
   ws.on("close", () => {

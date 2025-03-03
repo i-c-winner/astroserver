a7033a388925926a704465af9020a27223ba81ce
Dmitriy Belousov <d-belosov@hotmail.com>
2025-03-03 08:29:31 +0500

    feat: добавит offer

--- server.cjs
+++ server.cjs
@@ -7,13 +7,23 @@ const app = express();
 const server = http.createServer(app);
 const wss = new webSocket.WebSocketServer({server});
 const clients = new Map()
-
+let offer = null
 wss.on("connection", (ws) => {
   console.log("Клиент подключен");
   const id = Math.round(Math.random() * 1000);
   clients.set(id, ws);
+  if (offer) {
+    ws.send(JSON.stringify({
+      type: "offer",
+      payload: offer
+    }));
+  }
   ws.on("message", (message) => {
+
     const data = JSON.parse(message);
+    if (data.type === "offer") {
+      offer = data.payload;
+    }
     const target = clients.get(id);
     clients.forEach((ws, id) => {
       ws.send(JSON.stringify({...data, from: data.id}));

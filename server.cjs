fea57cf00514f5f012809db7f8e178bd4b88c589
Dmitriy Belousov <d-belosov@hotmail.com>
2025-03-04 04:04:42 +0500

    feat: change logic

--- server.cjs
+++ server.cjs
@@ -6,33 +6,50 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const wss = new webSocket.WebSocketServer({server});
-const clients = new Map()
-let offer = null
+const state={
+  offer: null,
+  offerId: 0,
+  clients: new Map(),
+  clientsSize: ()=> this.clients.size,
+  adminId: Math.round(Math.random() * 1000),
+}
 wss.on("connection", (ws) => {
-  console.log("Клиент подключен");
-  const id = Math.round(Math.random() * 1000);
-  clients.set(id, ws);
-  if (offer) {
+  const id=Math.round(Math.random()*1000);
+  if (state.clientsSize()>=3) {
     ws.send(JSON.stringify({
-      type: "offer",
-      payload: offer,
-      isOffer: true
+      type: "error",
+      payload: "full participants",
+      id: state.adminId
     }));
+    ws.close()
+    if (state.offer) {
+      ws.send(JSON.stringify({
+        type: "offer",
+        payload: state.offer,
+        isOffer: true,
+        id: state.adminId
+      }))
+    }
   }
   ws.on("message", (message) => {
-
     const data = JSON.parse(message);
-    if (data.type === "offer") {
-      offer = data.payload;
+    if (data.type === "offer"&&!state.offer) {
+      state.offer = data.payload;
+      state.offerId = data.id;
     }
-    const target = clients.get(id);
-    clients.forEach((ws, id) => {
-      ws.send(JSON.stringify({...data, from: data.id}));
+    const target = state.clients.get(id);
+    state.clients.forEach((ws, id) => {
+      target.send(JSON.stringify({...data, from: data.id}));
     })
     console.log(`Получено сообщение: ${message}`);
   });
 
   ws.on("close", () => {
+    if(state.offerId===id) {
+      state.offer=null
+      state.offerId=0
+    }
+
     console.log("Клиент отключился");
   });
 });

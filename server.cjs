4069d01c31685d1e088c3aba08d1efa7c1d30582
Dmitriy Belousov <d-belosov@hotmail.com>
2025-03-03 07:54:07 +0500

    fix: Изменит значения в clients

--- server.cjs
+++ server.cjs
@@ -19,7 +19,7 @@ wss.on("connection", (ws) => {
     console.log(data);
     console.log(clients);
     clients.forEach((ws, id) => {
-      ws.send(JSON.stringify({...data, from: id}));
+      ws.send(JSON.stringify({...data, from: data.id}));
     })
     console.log(`Получено сообщение: ${message}`);
   });
